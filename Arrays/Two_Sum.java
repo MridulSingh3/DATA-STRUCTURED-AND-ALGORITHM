7aecf934ada2ca1ff36d9e7539106677c4683ff2
Mridul Singh <mriduldhakad@gmail.com>
2026-06-02 09:34:04 +0530

    Update Two_Sum.java

--- Arrays/Two_Sum.java
+++ Arrays/Two_Sum.java
@@ -1,39 +1,39 @@
 import java.util.*;
 
 public class Main {
-    // Brute Force
+    //Brute Force
     public static int[] twoSumBrute(int[] nums, int target) {
-        int n = nums.length;
+        int n=nums.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
                 }
             }
         }
-        return new int[] { 0, 0 };
+        return new int[]{0,0};
     }
 
-    // Optimized
+    //Optimized
     public static int[] twoSumOptimized(int[] nums, int target) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
+        int n=nums.length;
+        Map<Integer,Integer>map=new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[] { map.get(target - nums[i]), i };
+        for(int i=0;i<n;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
             }
-            map.put(nums[i], i);
+            map.put(nums[i],i);
         }
-        return new int[] { 0, 0 };
+        return new int[]{0,0};
     }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 7, 11, 15 };
-        int target = 9;
+        int[]arr={2,7,11,15};
+        int target=9;
 
-        System.out.println(Arrays.toString(twoSumBrute(arr, target)));
-        System.out.println(Arrays.toString(twoSumOptimized(arr, target)));
+        System.out.println(Arrays.toString(twoSumBrute(arr,target)));
+        System.out.println(Arrays.toString(twoSumOptimized(arr,target)));
     }
 }
